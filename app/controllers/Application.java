5b59f9b097e74b316270bf5a8a38349a3f2790a8
Thomas Henrion <thomas.henrion@gmail.com>
2011-02-10 21:31:21 +0100

    first iteration of the authentication

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,10 +7,15 @@ import java.util.*;
 
 import models.*;
 
+@With(Secure.class)
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        if(Security.isConnected()) {
+			User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", user.fullname);
+        }
+		render();
     }
 
 }
