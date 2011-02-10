ad20bcb4619d0f719f8e273285e1a422d6247e65
Thomas Henrion <thomas.henrion@gmail.com>
2011-02-10 22:33:18 +0100

    add some tests for the User model

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -6,8 +6,27 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void createAndRetrieveUser() {
+        // Create a new user and save it
+        new User("bob@ctg.com", "secret", "Bob").save();
+
+        // Retrieve the user with e-mail address bob@gmail.com
+        User bob = User.find("byEmail", "bob@ctg.com").first();
+
+        // Test
+        assertNotNull(bob);
+        assertEquals("Bob", bob.fullname);
+
+        //cleanup
+        bob.delete();
+    }
+
+    @Test
+    public void tryConnectAsUser() {
+        // Test
+        assertNotNull(User.connect("alexis.paris@ctg.com", "secret"));
+        assertNull(User.connect("alexis.paris@ctg.com", "badpassword"));
+        assertNull(User.connect("thomass.henrion@ctg.com", "secret"));
     }
 
 }
